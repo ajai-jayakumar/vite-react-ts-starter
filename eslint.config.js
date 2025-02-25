66f0e3eb52f000a775abc8ddda8540ffe2f1b533
ajai-jayakumar <ajai199@gmail.com>
2025-02-25 16:40:52 +0100

    chore: add lint rule to sort the imports by group

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import prettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -17,6 +18,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,6 +26,27 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          // Define groups for sorting imports
+          groups: [
+            // Group 1: React and other third-party libraries
+            ['^react', '^@?\\w'],
+            // Group 2: Internal imports from specific directories (e.g., @, components)
+            ['^(@|components)(/.*|$)'],
+            // Group 3: Side-effect imports (e.g., CSS files)
+            ['^\\u0000'],
+            // Group 4: Parent imports (up one or two levels)
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Group 5: Same-folder imports (relative imports with subfolders)
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Group 6: Style imports (e.g., CSS files)
+            ['^.+\\.?(css)$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
     },
   },
   prettier
