a77a48d6ce4538830403e730d9641b0f99bbab62
ajai-jayakumar <ajai199@gmail.com>
2025-03-07 18:59:34 +0100

    chore: move input.test.tsx file inside input folder

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Input } from './index';
+import { Input } from '../index';
 
 describe('Input Component', () => {
   it('renders the component', () => {
